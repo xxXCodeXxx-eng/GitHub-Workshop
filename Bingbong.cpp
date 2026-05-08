c2702c01fcabbd6fe8df1dd4d7ced2b2f75b86a0
Johann <johannwbid@gmail.com>
2026-05-08 14:19:34 +0800

    New test

---
+++ Bingbong.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Tonny boyyy, Tonny boyyy, Tonny boyyy, Tonny boyyy, Tonny boyyy, Tonny boyyy, Tonny!!! boyyy,";
+  cout << "Tonny diffed";
+  return 0;
+}
