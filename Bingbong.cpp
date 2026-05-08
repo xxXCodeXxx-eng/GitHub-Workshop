257b4d376469cf780902dd9937b804b750431055
Johann <johannwbid@gmail.com>
2026-05-08 14:24:09 +0800

    I am tonny bot

--- Bingbong.cpp
+++ Bingbong.cpp
@@ -6,5 +6,6 @@ int main()
 {
   cout << "Tonny boyyy, Tonny boyyy, Tonny boyyy, Tonny boyyy, Tonny boyyy, Tonny boyyy, Tonny!!! boyyy,";
   cout << "Tonny diffed";
+  cout << "My bouy carly";
   return 0;
 }
